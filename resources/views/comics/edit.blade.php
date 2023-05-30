da5ac835a94c10a0332e9ab96bece33e04b4b5a0
Giadagenovesi <giada.g@outlook.it>
2023-05-30 16:42:00 +0200

    Completed  UPDATE and DELETE

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container py-4">
+        <h2>Inserisci un nuovo fumetto</h2>
+
+        <form action="{{ route('comics.update', $comic->id) }}" method="POST">
+            @method('PUT')
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="thumb" class="form-label">Immagine</label>
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{ $comic->thumb }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="type" class="form-label">Tipologia</label>
+                <select id="type" name="type" class="form-select">
+                    <option>Seleziona</option>
+                    <option  @selected($comic->type === 'graphic novel') value="graphic novel">graphic novel</option>
+                    <option  @selected($comic->type === 'comic book') value="comic book">comic book</option>
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="series" class="form-label">Serie</label>
+                <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="sale_date" class="form-label">Data di Uscita</label>
+                <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="price" class="form-label">Prezzo</label>
+                <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">Descrizione</label>
+                <textarea class="form-control" name="description" id="description" rows="3"> value="{{ $comic->description }}" </textarea>
+            </div>
+
+            <button type="submit" class="btn btn-success">Invia</button>
+
+        </form>
+    </div>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,7 +14,7 @@
                 <th scope="col">Id</th>
                 <th scope="col">Titolo</th>
                 <th scope="col">Tipologia</th>
-                <th scope="col">Dettagli</th>
+                <th scope="col">Azioni</th>
               </tr>
             </thead>
             <tbody>
@@ -27,6 +27,17 @@
                             <a href="{{route('comics.show', $comic->id)}}" class="btn btn-success">
                                 <i class="fa-solid fa-circle-info"></i>
                             </a>
+                            <a class="btn btn-warning" href="{{ route('comics.edit', $comic->id) }}">
+                                <i class="fa-solid fa-pen-to-square"></i>
+                            </a>
+
+                            <form class="d-inline-block" action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger">
+                                    <i class="fa-solid fa-trash"></i>
+                                </button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
