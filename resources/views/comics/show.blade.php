78e2b623ca1fc40ca47fff8f6ec6d8eeb60f4e6e
Giadagenovesi <giada.g@outlook.it>
2023-06-01 16:40:41 +0200

    Add some buttons Completed exercise with BONUS

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <div class="container py-4">
+        <a class="btn btn-success my-4" href="{{ route('comics.index') }}">Torna alla lista di fumetti</a>
         <h2>Dettagli: {{$comic->title}} </h2>
         <img class="w-25" src="{{$comic->thumb}}" alt="{{$comic->title}}">
         <p> Prezzo: {{$comic->price}} </p>
